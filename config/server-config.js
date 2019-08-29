9216225587f4b0494c02c05e267fdbb97fef1d02
thiago <thiago.massuia@hotmail.com>
2019-08-29 15:45:38 -0300

    test config

--- config/server-config.js
+++ config/server-config.js
@@ -6,6 +6,6 @@ module.exports = {
   fsRoot: path.resolve('./test-fiiiiles'),
   rootName: 'Customization area',
   readOnly: false,
-  port: process.env.PORT || '3020',
-  host: process.env.HOST || 'localhost'
+  port: process.env.PORT || '5000',
+  host: process.env.HOST || '178.128.128.41'
 };
