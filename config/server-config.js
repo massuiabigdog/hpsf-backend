a3133ca8524ce8e01ff629b80b75c6dca3a20d0d
thiago <thiago.massuia@hotmail.com>
2019-08-29 15:59:24 -0300

    localhost

--- config/server-config.js
+++ config/server-config.js
@@ -7,5 +7,5 @@ module.exports = {
   rootName: 'Customization area',
   readOnly: false,
   port: process.env.PORT || '5000',
-  host: process.env.HOST || '178.128.128.41'
+  host: process.env.HOST || 'localhost'
 };
